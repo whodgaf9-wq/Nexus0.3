0f064dce8ab23311feaa60b92fc7586de7351c9e
whodgaf9-wq <whodgaf9@gmail.com>
2026-08-03 21:24:27 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NexusDemo from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NexusDemo />
+  </React.StrictMode>,
+)
